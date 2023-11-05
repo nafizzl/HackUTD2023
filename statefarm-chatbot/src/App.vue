c0c19558412324fba632abbd80cc57b94c1dffd6
MurtazaKhan24 <khanmurtaza001@gmail.com>
2023-11-05 01:24:39 -0500

    added chatbot

--- statefarm-chatbot/src/App.vue
+++ statefarm-chatbot/src/App.vue
@@ -1,21 +1,18 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+ <Chat />
 </template>
+
+<script>
+import Chat from './components/Chat.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Chat
+  }
+}
+
+</script>
 
 <style scoped>
 header {
